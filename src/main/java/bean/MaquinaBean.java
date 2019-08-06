1e2facf0a6cd4c07946a2b60db59b09755a6992a
jorgegomesen <jorgegomesen@gmail.com>
2019-08-06 10:32:14 -0300

    Alterações nos layouts

---
+++ src/main/java/bean/MaquinaBean.java
@@ -0,0 +1,51 @@
+package bean;
+
+public class MaquinaBean {
+	private int n_registro;
+	private String marca;
+	private float potencia;
+	private float consumo;
+	private float valor_diaria;
+	private String tipo_combustivel;
+	
+	public int getN_registro() {
+		return n_registro;
+	}
+	public void setN_registro(int n_registro) {
+		this.n_registro = n_registro;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public float getPotencia() {
+		return potencia;
+	}
+	public void setPotencia(float potencia) {
+		this.potencia = potencia;
+	}
+	public float getConsumo() {
+		return consumo;
+	}
+	public void setConsumo(float consumo) {
+		this.consumo = consumo;
+	}
+	public float getValor_diaria() {
+		return valor_diaria;
+	}
+	public void setValor_diaria(float valor_diaria) {
+		this.valor_diaria = valor_diaria;
+	}
+	public String getTipo_combustivel() {
+		return tipo_combustivel;
+	}
+	public void setTipo_combustivel(String tipo_combustivel) {
+		this.tipo_combustivel = tipo_combustivel;
+	}
+	public String toString() {
+		return "";
+	}
+
+}
