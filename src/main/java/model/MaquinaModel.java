5d2a816e2bc9636287cbc704dbc0534407c8be05
jorgegomesen <jorgegomesen@gmail.com>
2019-08-06 18:59:13 -0300

    Mudanças no layout

--- src/main/java/model/MaquinaModel.java
+++ src/main/java/model/MaquinaModel.java
@@ -12,36 +12,29 @@ import resources.Database;
 
 public class MaquinaModel {
 
-	public static void create() {
+	public void create(MaquinaBean maquina) {
 		Database db = new Database();
 		Connection conn = null;
 
 		conn = db.getConnection();
-
-		MaquinaBean maquina = new MaquinaBean();
-		maquina.setMarca("asdadasdsad");
-		maquina.setTipo_combustivel("klajlsdjka");
-		maquina.setConsumo(3.43f);
-		maquina.setN_registro(987);
-		maquina.setValor_diaria(98.2f);
+//
+//		MaquinaBean maquina = new MaquinaBean();
+//		maquina.setMarca("asdadasdsad");
+//		maquina.setTipo_combustivel("klajlsdjka");
+//		maquina.setConsumo(3.43f);
+//		maquina.setN_registro(987);
+//		maquina.setValor_diaria(98.2f);
 
 		try {
 			if (conn != null) {
 
 				PreparedStatement st = conn.prepareStatement("EXECUTE PROCEDURE MAQUINA_CREATE(?,?,?,?,?)");
-//				PreparedStatement st = conn
-//						.prepareStatement("INSERT INTO MAQUINA (N_REGISTRO, MARCA, POTENCIA, VAL_DIARIA, "
-//								+ "TIPO_COMBUST) VALUES (?, ?, ?, ?, ?)");
+				
 				st.setString(1, "" + maquina.getN_registro());
 				st.setString(2, maquina.getMarca());
 				st.setString(3, "" + maquina.getPotencia());
 				st.setString(4, "" + maquina.getValor_diaria());
 				st.setString(5, maquina.getTipo_combustivel());
-
-//				st.setInt(1, Integer.parseInt(maquina.getUs_vendedor()));
-//				st.setInt(2, Integer.parseInt(maquina.getUs_comprador()));
-//				st.setString(3, maquina.getData());
-//				st.setFloat(4, Float.parseFloat(maquina.getValor()));
 
 				st.execute();
 
@@ -53,8 +46,8 @@ public class MaquinaModel {
 		}
 	}
 
-	public static void main(String[] args) {
-		create();
-	}
+//	public static void main(String[] args) {
+//		create();
+//	}
 
 }
